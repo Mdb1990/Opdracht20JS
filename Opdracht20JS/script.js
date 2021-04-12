35d623c15c7598087cad14b641d38975b124a328
Marco de boer <76598975+Mdb1990@users.noreply.github.com>
2021-04-12 21:51:35 +0200

    Add files via upload

---
+++ Opdracht20JS/script.js
@@ -0,0 +1,15 @@
+
+console.log("Hallo Winc Academy studenten");
+
+// 1230941 x 1823794 dit is een grote som
+
+/*let a = 1230941
+let b = 1823794
+let sum = a * b
+console.log("sum answer:", sum);
+
+let c = 637263
+let d = 54
+let sum2 = c / d
+console.log("sum2 answer:", sum2);
+*/
